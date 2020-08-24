7497740671ca013fd3f1bc1f30e8dc1e2f5269bc
jesusvillalbam@gmail.com <jevillalba@personalsoft.com.co>
2020-08-24 11:09:31 -0500

    commit inicial

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+// Application name 	PlatziMusic
+// API key 	91d8767f76b54f48de869a45be113c4f
+// Shared secret 	364c06ffd58ee8ce70175224bf426dde
+// Registered to 	jesusvillalbam
+
+export default {
+    appName: 'PlatziMusic',
+    apiKey: '91d8767f76b54f48de869a45be113c4f',
+    secret: '364c06ffd58ee8ce70175224bf426dde',
+    registeredTo: 'jesusvillalbam'
+}
